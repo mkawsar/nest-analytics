1e01fa5b1dbd1fab25f701967f3f0f0c693c40e4
mkawsar <kawsar.diu888@gmail.com>
2024-09-24 20:12:52 +0600

    added passport local

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,6 @@
 import * as bcrypt from 'bcrypt';
 import { UserService } from '../user/user.service';
-import { forwardRef, Inject, Injectable } from '@nestjs/common';
+import { forwardRef, Inject, Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
 
 @Injectable()
 export class AuthService {
@@ -21,5 +21,26 @@ export class AuthService {
                 resolve(hash);
             });
         });
+    }
+
+    async validateUser(email: string, pass: string): Promise<any> {
+        const query = {email: email};
+        const user = await this.UserService.findOne(query);
+        if (!user) throw new NotFoundException('Email Does not exist');
+        const isMatched = this.comparedPassword(pass, user?.password);
+        if (!isMatched) throw new UnauthorizedException('Invalid Password');
+        return user;
+    }
+
+    // Compared password
+    async comparedPassword(password: string, hashedPassword: string): Promise<any>{
+        return bcrypt
+            .compare(password, hashedPassword)
+            .then((isMatched: any) => {
+                if (isMatched) return true;
+                return false;
+            }).catch((err: any) => {
+                err;
+            });
     }
 }
