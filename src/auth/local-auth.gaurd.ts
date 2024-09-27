43853b55ba921bed90821b5ba3519118fe9ba652
mkawsar <kawsar.diu888@gmail.com>
2024-09-27 19:58:56 +0600

    removed extra line

--- src/auth/local-auth.gaurd.ts
+++ src/auth/local-auth.gaurd.ts
@@ -1,11 +1,9 @@
 import { AuthGuard } from '@nestjs/passport';
 import { Injectable, HttpException, ExecutionContext } from '@nestjs/common';
 
-
 @Injectable()
 export class LocalAuthGaurd extends AuthGuard('local') {
     handleRequest(err: any, user: any, info: any, context: ExecutionContext, status?: any) {
-        console.log(context);
         if (err || !user) {
             throw new HttpException(err?.message, err?.status);
         }
