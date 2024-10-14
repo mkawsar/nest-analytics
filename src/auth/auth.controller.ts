a236f3c38695ff7a14e94128a04ff3da76432a21
mkawsar <kawsar.diu888@gmail.com>
2024-10-14 15:35:14 +0600

    use role guard for request

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,9 @@
+import { Roles } from './roles.decorator';
+import { RolesGuard } from './roles.guard';
 import { AuthService } from './auth.service';
 import { PostLoginDto } from './auth.decorator';
 import { JwtAuthGaurd } from './jwt-auth.gaurd';
+import { Role } from 'src/user/enums/role.enum';
 import { LocalAuthGaurd } from './local-auth.gaurd';
 import { Controller, Post, Logger, Request, UseGuards, Body, Get } from '@nestjs/common';
 
@@ -15,13 +18,15 @@ export class AuthController {
     @UseGuards(LocalAuthGaurd)
     async login(@Request() req, @Body() dto: PostLoginDto): Promise<any> {
         try {
-            return this.authService.generateJwtToken(req.user);
+            return this.authService.generateJwtToken(dto);
         } catch (err) {
             throw err;
         }
     }
 
     @Get('view/profile')
+    @Roles(Role.Admin, Role.User, Role.Moderator)
+    @UseGuards(RolesGuard)
     @UseGuards(JwtAuthGaurd)
     async getAuthUserProfile(@Request() req): Promise<any> {
         return req.user;
--- src/auth/roles.guard.ts
+++ src/auth/roles.guard.ts
@@ -6,7 +6,6 @@ import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 export class RolesGuard implements CanActivate {
     constructor(private reflactor: Reflector) {}
     
-
     canActivate(context: ExecutionContext): boolean {
         const requiredRoles = this.reflactor.get<Role[]>('roles', context.getHandler());
         if (!requiredRoles) {
@@ -15,6 +14,7 @@ export class RolesGuard implements CanActivate {
 
         const request = context.switchToHttp().getRequest();
         const user = request.user;
+        
         return requiredRoles.some((role) => user.role.includes(role));
     }
 }
