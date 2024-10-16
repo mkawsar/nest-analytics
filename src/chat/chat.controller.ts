708ee1b0f80d41c284758f2451b0818e71869e77
mkawsar <kawsar.diu888@gmail.com>
2024-10-16 12:30:17 +0600

    integrated chat app

---
+++ src/chat/chat.controller.ts
@@ -0,0 +1,8 @@
+import { Controller } from '@nestjs/common';
+import { ChatService } from './chat.service';
+
+@Controller('chat')
+export class ChatController {
+    constructor(private readonly chatService: ChatService) {
+    }
+}
