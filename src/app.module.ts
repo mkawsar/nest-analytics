494c027fb4b39bcbf1b308824bb8dcd6bfabcd88
mkawsar <kawsar.diu888@gmail.com>
2024-10-29 14:54:34 +0600

    deleted chat app

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import { ConfigModule } from '@nestjs/config';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
-import { ChatModule } from './chat/chat.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { RoomsModule } from './rooms/rooms.module';
 import { SuccessResponseIntercptor } from './services/success.response.interceptor';
@@ -16,7 +15,6 @@ import { SuccessResponseIntercptor } from './services/success.response.intercept
         }),
         UserModule,
         AuthModule,
-        ChatModule,
         RoomsModule
     ],
     controllers: [],
