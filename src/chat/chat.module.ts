7bed9a697adbdb49c84ede2c528eb06f65c0fd1f
mkawsar <kawsar.diu888@gmail.com>
2024-11-02 20:18:10 +0600

    message model added

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ChatService } from './chat.service';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ChatController } from './chat.controller';
+import { Message, MessageSchema } from './schemas/message.model';
 
 @Module({
+    imports: [
+        MongooseModule.forFeature([{ name: Message.name, schema: MessageSchema }]),
+    ],
     controllers: [ChatController],
     providers: [ChatService],
 })
